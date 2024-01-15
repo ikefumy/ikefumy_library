cabcfab7f60b0297f69983b55e45bf2b11a0e4dc
ikefumy <ikeke0254@gmail.com>
2024-01-15 18:09:13 +0900

    fix: SCCでios～が二回繰り返されてたので消した

--- test/LibraryChecker/strongly_connected_components.test.cpp
+++ test/LibraryChecker/strongly_connected_components.test.cpp
@@ -9,9 +9,6 @@ int main() {
     cin.tie(nullptr);
     ios_base::sync_with_stdio(false);
     cout << fixed << setprecision(20);
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    cout << fixed << setprecision(20);
     cin >> N >> M;
     strongly_connected_components g(N);
     rep (i, M) {
