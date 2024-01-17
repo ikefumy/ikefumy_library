d173fc2575c723924e07d0fdf8bd679db76be985
ikefumy <ikeke0254@gmail.com>
2024-01-17 11:45:49 +0900

    test: enumerate quotients

---
+++ test/LibraryChecker/enumerate_quotients.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/enumerate_quotients"
+
+#include "../../template/template.hpp"
+#include "../../math/quotient_range.hpp"
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    ll n;
+    cin >> n;
+    auto qr = quotient_range(n);
+    cout << qr.size() << '\n';
+    reverse(all(qr));
+    for (auto [l, r, q] : qr) cout << q << ' ';
+    cout << '\n';
+}
