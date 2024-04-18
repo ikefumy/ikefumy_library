3f6a998fe24f23c9092b76b6aa456f580ff1ca40
ikefumy <ikeke0254@gmail.com>
2024-04-18 09:57:59 +0900

    フロー周りを追加

---
+++ graph/max_flow_lb.hpp
@@ -0,0 +1,26 @@
+template<class F>
+struct max_flow_lb {
+    Dinic<F> f;
+    int S, T;
+    F sum_lb;
+
+    max_flow_lb (int n) : f(n + 2), S(n), T(n + 1), sum_lb(0) {}
+
+    void add_edge(int u, int v, int lb, int ub) {
+        assert(0 <= lb && lb <= ub);
+        if (ub == 0) return;
+        f.add_edge(u, v, ub - lb);
+        if (lb == 0) return;
+        sum_lb += lb;
+        f.add_edge(S, v, lb);
+        f.add_edge(u, T, lb);
+    }
+
+    F max_flow(int s, int t) {
+        F a = f.max_flow(S, T);
+        F b = f.max_flow(s, T);
+        F c = f.max_flow(S, t);
+        F d = f.max_flow(s, t);
+        return (a + b == sum_lb && a + c == sum_lb) ? b + d : -1;
+    }
+};
---
+++ test/AOJ/1615.cpp
@@ -0,0 +1,42 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/1615"
+
+#include "../../template/template.hpp"
+#include "../../graph/dinic.hpp"
+#include "../../graph/max_flow_lb.hpp"
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(20);
+    int n, m;
+    while (true) {
+        cin >> n >> m;
+        if (n == 0) break;
+        vector<int> u(m), v(m);
+        rep (i, m) cin >> u[i] >> v[i], u[i]--, v[i]--;
+        bool flag = false;
+        int amn = 0, amx = inf;
+        int mn = 0;
+        rep (mx, 1, n + 1) {
+            while (mn <= mx) {
+                max_flow_lb<int> mfl(n + m + 2);
+                int s = n + m, t = n + m + 1;
+                rep (i, n) {
+                    mfl.add_edge(m + i, t, mn, mx);
+                }
+                rep (i, m) {
+                    mfl.add_edge(s, i, 0, 1);
+                    mfl.add_edge(i, m + u[i], 0, 1);
+                    mfl.add_edge(i, m + v[i], 0, 1);
+                }
+                if (mfl.max_flow(s, t) == m && mx - mn <= amx - amn) {
+                    amx = mx, amn = mn;
+                    mn++;
+                } else {
+                    break;
+                }
+            }
+        }
+        cout << amn << ' ' << amx << '\n';
+    }
+}
